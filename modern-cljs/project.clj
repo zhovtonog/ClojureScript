4db73d0c054abfb390ab18b2d4a24c1d9015a355
azhovtonog <azhovtonog@gfl-1604.geeksforless.net>
2016-05-06 20:46:33 +0300

    first end

---
+++ modern-cljs/project.clj
@@ -0,0 +1,27 @@
+(defproject modern-cljs "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :source-paths ["src/clj" "src/cljs"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.145"]]
+
+  :plugins [[lein-cljsbuild "1.1.0"]]
+
+  :cljsbuild {:builds
+              [{;; CLJS source code path
+                :source-paths ["src/cljs"]
+
+                ;; Google Closure (CLS) options configuration
+                :compiler {;; CLS generated JS script filename
+                           :output-to "resources/public/js/modern.js"
+
+                           ;; minimal JS optimization directive
+                           :optimizations :whitespace
+
+                           ;; generated JS code prettyfication
+                           :pretty-print true}}]}
+  ;; to clean JS files generated during the build
+  :clean-targets ^{:protect false} [:target-path "resources/public/js/"])
